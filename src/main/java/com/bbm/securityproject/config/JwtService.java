050427a9ab51c9ffd408caa9665694802f234ab9
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-06-19 23:42:05 +0200

    A service to extract a userEmail

---
+++ src/main/java/com/bbm/securityproject/config/JwtService.java
@@ -0,0 +1,10 @@
+package com.bbm.securityproject.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUserEmail(String token) {
+        return null;
+    }
+}
