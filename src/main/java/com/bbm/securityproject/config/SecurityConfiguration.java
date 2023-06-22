d5d155535c43b37f547a77626961d91bb4d63bbd
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-06-22 20:37:18 +0200

    getting the authorities

--- src/main/java/com/bbm/securityproject/config/SecurityConfiguration.java
+++ src/main/java/com/bbm/securityproject/config/SecurityConfiguration.java
@@ -1,10 +1,8 @@
 package com.bbm.securityproject.config;
 
-import com.bbm.securityproject.model.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
