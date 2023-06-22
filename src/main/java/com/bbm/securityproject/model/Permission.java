80178615504bec615006b5b7ea376610d455c7d0
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-06-22 20:34:12 +0200

    Adding the admin and manager Permissions

---
+++ src/main/java/com/bbm/securityproject/model/Permission.java
@@ -0,0 +1,18 @@
+package com.bbm.securityproject.model;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Permission {
+
+    ADMIN_CREATE("admin:create"),
+    ADMIN_READ("admin:read"),
+    ADMIN_UPDATE("admin:update"),
+    ADMIN_DELETE("admin:delete"),
+    MANAGER_CREATE("management:create"),
+    MANAGER_READ("management:read"),
+    MANAGER_UPDATE("management:update"),
+    MANAGER_DELETE("management:delete");
+
+    private final String permission;
+}
