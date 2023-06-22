d5cca3bbe6a99588c050af978fe73339e1055ea0
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-06-22 19:44:27 +0200

    Adding a controllers to test the Roles and Permissions

---
+++ src/main/java/com/bbm/securityproject/controller/ManagementController.java
@@ -0,0 +1,28 @@
+package com.bbm.securityproject.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/management")
+public class ManagementController {
+
+    @PostMapping
+    public String post() {
+        return "POST:: management controller";
+    }
+
+    @GetMapping
+    public String get() {
+        return "GET:: management controller";
+    }
+
+    @PutMapping
+    public String put() {
+        return "PUT:: management controller";
+    }
+
+    @DeleteMapping
+    public String delete() {
+        return "DELETE:: management controller";
+    }
+}
