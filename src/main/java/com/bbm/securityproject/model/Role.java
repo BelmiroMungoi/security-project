2015c94b825446819ef0aa335c5e2c416d9eed9f
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-06-22 20:38:12 +0200

    setting the admin and manager authorities

--- src/main/java/com/bbm/securityproject/model/Role.java
+++ src/main/java/com/bbm/securityproject/model/Role.java
@@ -1,6 +1,49 @@
 package com.bbm.securityproject.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.bbm.securityproject.model.Permission.*;
+
+@RequiredArgsConstructor
 public enum Role {
 
-    USER, ADMIN
+    USER(Set.of()),
+    ADMIN(
+            Set.of(
+                    ADMIN_CREATE,
+                    ADMIN_READ,
+                    ADMIN_UPDATE,
+                    ADMIN_DELETE,
+                    MANAGER_CREATE,
+                    MANAGER_READ,
+                    MANAGER_UPDATE,
+                    MANAGER_DELETE)
+    ),
+    MANAGER(
+            Set.of(
+                    MANAGER_CREATE,
+                    MANAGER_READ,
+                    MANAGER_UPDATE,
+                    MANAGER_DELETE
+            )
+    );
+
+    @Getter
+    private final Set<Permission> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        var authorities = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.name()))
+                .toList();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" +this.name()));
+        return authorities;
+    }
 }
