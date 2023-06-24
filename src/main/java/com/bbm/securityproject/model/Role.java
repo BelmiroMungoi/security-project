cb2da3affc1a44b042a1bf37bf8b1739ac55556b
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-06-24 11:31:00 +0200

    changing the method to collect toList

--- src/main/java/com/bbm/securityproject/model/Role.java
+++ src/main/java/com/bbm/securityproject/model/Role.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -41,8 +40,8 @@ public enum Role {
     public List<SimpleGrantedAuthority> getAuthorities() {
         var authorities = getPermissions()
                 .stream()
-                .map(permission -> new SimpleGrantedAuthority(permission.name()))
-                .toList();
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toList());
         authorities.add(new SimpleGrantedAuthority("ROLE_" +this.name()));
         return authorities;
     }
