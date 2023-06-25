2b8d1eb5e97bc40a053edf23778a27dbb95d324a
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-06-25 09:48:08 +0200

    adding a token class

---
+++ src/main/java/com/bbm/securityproject/model/Token.java
@@ -0,0 +1,31 @@
+package com.bbm.securityproject.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EnableMethodSecurity
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType;
+    private boolean expired;
+    private boolean revoked;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private  User user;
+}
